c3dfd2a90eab0d97f17b6ac2ea78f5c310338a4c
finnradatz <radatzfinn@web.de>
2023-03-14 21:40:45 +0100

    salary verifier

--- src/excelchaos_model/inputVerifier/SalaryVerifier.java
+++ src/excelchaos_model/inputVerifier/SalaryVerifier.java
@@ -12,19 +12,19 @@ public class SalaryVerifier extends InputVerifier {
         String text = ((JTextField) input).getText();
         if (text.matches("")) {
             return true;
-        } else if (text.matches("^\\d+(\\.\\d+)*$")) {
+        } else if (text.matches("(?:\\d+\\.*\\d+)")) {
             ((JTextField) input).setText(text + ",00" + " €");
             return true;
-        } else if (text.matches("^\\d+(\\.\\d+)*$,\\d{2}") || text.matches("^\\d+(\\.\\d+)*$\\.\\d{2}")) {
-            ((JTextField) input).setText(text.replaceAll("\\.",",") + " €");
+        } else if (text.matches("(?:\\d+\\.*\\d+),\\d{2}") || text.matches("(?:\\d+\\.*\\d+)\\.\\d{2}")) {
+            ((JTextField) input).setText(text.replaceFirst("(?s)(.*)" + "\\.","$1" + ",") + " €");
             return true;
-        } else if (text.matches("^\\d+(\\.\\d+)*$,\\d{1}") || text.matches("^\\d+(\\.\\d+)*$\\.\\d{1}")) {
-            ((JTextField) input).setText(text.replaceAll("\\.",",0") + " €");
+        } else if (text.matches("(?:\\d+\\.*\\d+),\\d{1}") || text.matches("(?:\\d+\\.*\\d+)\\.\\d{1}")) {
+            ((JTextField) input).setText(text.replaceFirst("(?s)(.*)" + "\\.","$1" + ",0") + " €");
             return true;
-        } else if (text.matches("^\\d+(\\.\\d+)*$\\.\\d{2}\\s€")) {
-            ((JTextField) input).setText(text.replaceAll("\\.",",") + " €");
+        } else if (text.matches("(?:\\d+\\.*\\d+)\\.\\d{2}\\s€")) {
+            ((JTextField) input).setText(text.replaceFirst("(?s)(.*)" + "\\.","$1" + ",0") + " €");
             return true;
-        } else if (text.matches("^\\d+(\\.\\d+)*$,\\d{2}\\s€")) {
+        } else if (text.matches("(?:\\d+\\.*\\d+),\\d{2}\\s€")) {
             return true;
         } else {
             JOptionPane.showConfirmDialog(null, "Bitte geben Sie einen Geldwert ein und trennen Sie falls nötig die Nachkommastellen mit \",\" ab.", "Falsches Eingabe Format", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE);
