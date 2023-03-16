4baed06a95ec5fab90a3e1744a235394d14343d2
Hieu Le <minhhieu.le@stud.tu-darmstadt.de>
2023-03-16 22:38:28 +0100

    Add comment to the ToolbarShowSHKTableView class

--- src/excelchaos_view/toolbarviews/ToolbarShowSHKTableView.java
+++ src/excelchaos_view/toolbarviews/ToolbarShowSHKTableView.java
@@ -7,13 +7,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemListener;
-
+/**
+ * ToolbarShowSHKTableView is a class that extends the SearchPanelToolbar class to create a toolbar with search panel for managing
+ * SHK table's data in the ShowSHKTableView class. It provides buttons for adding entry, editing entry and delete entry from the SHK Table
+ *
+ * @see excelchaos_view.ShowSHKTableView
+ */
 public class ToolbarShowSHKTableView extends SearchPanelToolbar {
 
     private JButton addEntry;
     private JButton editEntry;
     private JButton deleteEntry;
 
+    /**
+     * Initializes the view by setting up the toolbar components and adding them to the view. Background color, layout and floatability is also setup
+     * The buttons deleteEntry, editEntry are set unenabled on start-up
+     */
     public void init() {
         setFloatable(false);
         setBackground(Color.WHITE);
@@ -29,21 +38,31 @@ public class ToolbarShowSHKTableView extends SearchPanelToolbar {
         deleteEntry.setEnabled(false);
         setUpSearchPanel();
     }
-
+    /**
+     * Sets the same ActionListener for the buttons in the toolbar, allowing them to share the same controller which control event handling
+     * @see excelchaos_controller.ToolbarShowProjectsController;
+     * @param l the ActionListener to be set
+     */
     public void setActionListener(ActionListener l) {
         addEntry.addActionListener(l);
         editEntry.addActionListener(l);
         deleteEntry.addActionListener(l);
     }
-
+    /**
+     * @return {@link #editEntry}
+     */
     public JButton getEditEntry() {
         return editEntry;
     }
-
+    /**
+     * @return {@link #addEntry}
+     */
     public JButton getAddEntry() {
         return addEntry;
     }
-
+    /**
+     * @return {@link #deleteEntry}
+     */
     public JButton getDeleteEntry() {
         return deleteEntry;
     }
